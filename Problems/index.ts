27edc51f0d5ac8e3fb57a1894141d86267cf2dd9
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-05-08 01:07:37 +0600

    solve problem 4, 5, 6, 7 and 8

--- Problems/index.ts
+++ Problems/index.ts
@@ -27,18 +27,138 @@
         { title: "Book A", rating: 4.5 },
         { title: "Book B", rating: 3.2 },
         { title: "Book C", rating: 5.0 }
-      ];
+    ];
 
-     const highRatedBooks = filterByRating(books)
-     console.log(highRatedBooks)
+    const highRatedBooks = filterByRating(books)
+    console.log(highRatedBooks)
 
     //  Problem-3
 
     const concatenateArrays = <T>(...arrays: T[][]): T[] => {
-                 return [].concat(...arrays)
+        return [].concat(...arrays)
     }
-    const array: string[] =   concatenateArrays([12],[8],[33]) 
-    console.log(array)
+    console.log(concatenateArrays([12], [8], [33]))
+
+    // problem -4
+
+    class Vehicle {
+        private make: string;
+                year: number;
+
+        constructor(make: string, year: number) {
+            this.make = make,
+                this.year = year
+        }
+
+        getInfo() {
+            return `Make: ${this.make}, Year: ${this.year}`
+        }
+    }
+
+    class Car extends Vehicle {
+        private model: string;
+
+        constructor(make: string, year: number, model: string) {
+            super(make, year),
+                this.model = model
+        }
+
+        getModel() {
+            return `Model: ${this.model}`
+        }
+    }
+
+    const myCar = new Car("Toyota", 2020, "Corolla");
+    myCar.getInfo();
+    myCar.getModel();
+    console.log(myCar.getInfo())
+    console.log(myCar.getModel())
+
+
+    // Problem-5
+    const processValue = (value: string | number): number | undefined => {
+        if (typeof value === "string") {
+            return value.length
+        }
+        else if (typeof value === "number") {
+            return value * 2
+        }
+
+    }
+
+    processValue("hello");
+    processValue(10);
+
+    console.log(processValue("hello"))
+    console.log(processValue(10))
+
+    // problem-6
+
+    interface Product {
+        name: string;
+        price: number;
+    }
+
+    const getMostExpensiveProduct = (products: Product[]): Product | null => {
+        if(products.length === 0) {
+            return null
+        }
+        const sortProduct = products.sort((a, b) => b.price - a.price)
+        return sortProduct[0]
+    }
+
+    const products = [
+        { name: "Pen", price: 10 },
+        { name: "Notebook", price: 25 },
+        { name: "Bag", price: 50 }
+    ];
+
+
+    console.log(getMostExpensiveProduct(products))
+    console.log(getMostExpensiveProduct([]))
+
+    //  Problem-7
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    const getDayType = (day: Day): string => {
+        if (day >= Day.Monday && day <= Day.Friday) {
+            return "Weekday"
+        } else {
+            return "Weekend"
+        }
+    }
+
+    getDayType(Day.Monday);   // Output: "Weekday"
+    getDayType(Day.Sunday);   // Output: "Weekend"
+    console.log(getDayType(Day.Monday))
+    console.log(getDayType(Day.Sunday))
+
+
+    //    problem-8
+
+    const squareAsync = async(n: number): Promise<number> => {
+                   return new Promise<number>((resolve, reject) => {
+                        setTimeout(() => {
+                              if(n < 0){
+                                return reject("Negative number not allowed")
+                              }else{
+                                return resolve(n * n)
+                              }
+                        }, 1000)
+                   }) 
+    }
+
+    squareAsync(5).then(console.log);        // Output after 1s: 16
+    squareAsync(-4).catch(console.error);    // Output: Error: Negative number not allowed
+    
 
     //
 }
